7dca8c08e4254e48ff5de492784c3bfd8990ff0a
Guojin Chen <cgjhaha@qq.com>
2017-03-29 11:05:46 +0800

    change a new screenshoot

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
 <div id="app">
   <mu-appbar>
     <mu-icon-button icon='menu' slot="left" />
-    <mu-flat-button slot="left" label="creadr"/>
-    <mu-flat-button slot="left" label="注音" @click="creadr"/>
-    <mu-flat-button slot="left" label="分词"/>
-    <mu-flat-button slot="left" label="翻译"/>
-    <mu-flat-button slot="left" label="解释"/>
+    <mu-flat-button slot="left" label="creadr" />
+    <mu-flat-button slot="left" label="注音" @click="creadr" />
+    <mu-flat-button slot="left" label="分词" />
+    <mu-flat-button slot="left" label="翻译" />
+    <mu-flat-button slot="left" label="解释" />
     <mu-icon-button icon='account_box' slot="right" />
   </mu-appbar>
   <div class="paper">
-  <mu-text-field class="my-text-field" hintText="输入字符" fullWidth multiLine :rows="6" v-model="msg" icon="comment" />
-  <br/>
-    <tb v-for="(article,index) in articleArr"   v-bind:style="{ color: article.color}" :data="article.word_obj">
-    </tb>
+    <mu-text-field class="my-text-field" hintText="输入字符" fullWidth multiLine :rows="6" v-model="msg" icon="comment" />
+    <br/>
+    <div class="results">
+      <tb v-for="(article,index) in articleArr" v-bind:style="{ color: article.color}" :data="article.word_obj">
+      </tb>
+    </div>
+
   </div>
 </div>
 </template>
@@ -27,9 +30,9 @@ export default {
     let articleArr = [];
     // let activeColor = "red";
     let touch = true,
-        show = false,
-        verticalPosition = "top",
-        horizontalPosition = "center";
+      show = false,
+      verticalPosition = "top",
+      horizontalPosition = "center";
     let trigger = null;
 
     return {
@@ -97,13 +100,17 @@ export default {
         overflow-y: auto;
     }
 
-    .paper{
-      width: 80%;
-      margin: 0 auto;
-      box-shadow: 0 0 10px  rgba(158, 158, 158, .5);
-      margin-bottom: 20px;
-      margin-top: 40px;
-      min-height: 200px;
+    .paper {
+        width: 80%;
+        margin: 0 auto;
+        box-shadow: 0 0 10px rgba(158, 158, 158, .5);
+        margin-bottom: 20px;
+        margin-top: 40px;
+        min-height: 200px;
+
+        .results{
+          padding-left: 56px;
+        }
     }
 
 }
